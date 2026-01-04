8c8588322081086cfd2f5e5c520dd3662884ea8a
Abdelhafid Guerouani <Abdelhafid.Guerouani@bordeaux-inp.fr>
2026-01-04 17:59:38 +0100

    improve last model

--- submissions/aguerouani/agent.py
+++ submissions/aguerouani/agent.py
@@ -12,12 +12,11 @@ from pathlib import Path
 class PredatorPolicy(nn.Module):
     def __init__(self, input_dim=16, output_dim=5, hidden_dim=64):
         super().__init__()
-        # Reproduit exactement SB3 policy
         self.policy_net = nn.Sequential(
             nn.Linear(input_dim, hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim, hidden_dim),
-            nn.ReLU()
+            nn.ReLU(),
         )
         self.action_net = nn.Linear(hidden_dim, output_dim)
 
